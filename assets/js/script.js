435782218dcce594ac79b48583db8b096ebaf1fd
IrsyadHaniff <irsyadhanif332@gmail.com>
2023-09-11 12:52:55 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,379 @@
+"use strict";
+
+/* |-all music information-| */
+const musicData = [
+  {
+    backgroundImage: "./assets/images/poster-1.jpg",
+    posterUrl: "./assets/images/poster-1.jpg",
+    title: "Bingung Pilih Yang Mana (Tiktok)",
+    album: "No Spirit",
+    year: 2022,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music1.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-2.jpg",
+    posterUrl: "./assets/images/poster-2.jpg",
+    title: "Dont Give Up (SpeedUp)",
+    album: "Album",
+    year: 2023,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music2.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-3.jpg",
+    posterUrl: "./assets/images/spongebob.gif",
+    title: "All The Kids All Deppres",
+    album: "Album",
+    year: 2023,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music3.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-2.jpg",
+    posterUrl: "./assets/images/giphy.gif",
+    title: "Weenakk(Tiktok)",
+    album: "Not Found",
+    year: 2023,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music4.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-5.jpg",
+    posterUrl: "./assets/images/poster-5.jpg",
+    title: "Let Me Down Slowly",
+    album: "Album",
+    year: 2023,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music5.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-2.jpg",
+    posterUrl: "./assets/images/poster-2.jpg",
+    title: "Sayang (Tiktok)",
+    album: "Not Found",
+    year: 2022,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music6.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-5.jpg",
+    posterUrl: "./assets/images/poster-5.jpg",
+    title: "Bingung Pilih Yang Mana (Tiktok)",
+    album: "Not Found",
+    year: 2022,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music7.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-3.jpg",
+    posterUrl: "./assets/images/poster-3.jpg",
+    title: "Bingung Pilih Yang Mana (Tiktok)",
+    album: "Not Found",
+    year: 2022,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music8.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-1.jpg",
+    posterUrl: "./assets/images/poster-5.jpg",
+    title: "Bingung Pilih Yang Mana (Tiktok)",
+    album: "Not Found",
+    year: 2022,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music9.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-3.jpg",
+    posterUrl: "./assets/images/poster-4.jpg",
+    title: "Bingung Pilih Yang Mana (Tiktok)",
+    album: "No Spirit",
+    year: 2022,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music10.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-2.jpg",
+    posterUrl: "./assets/images/poster-2.jpg",
+    title: "Pyphone (Tiktok)",
+    album: "Not Found",
+    year: 2022,
+    artist: "Irsyad Hanif",
+    musicPath: "./assets/music/music11.mp3",
+  },
+];
+
+/*tambahkan event Listener pada semua elemen yang diteruskan */
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+/* PLAYLIST
+ - tambahkan semua musik dalam daftar putar, dari 'musicData' */
+
+const playlist = document.querySelector("[data-music-list]");
+
+for (let i = 0, len = musicData.length; i < len; i++) {
+  playlist.innerHTML += `
+  <li>
+    <button class="music-item ${i === 0 ? "playing" : ""}" data-playlist-toggler data-playlist-item="${i}">
+      <img src="${musicData[i].posterUrl}" width="800" height="800" alt="${musicData[i].title} Album Poster"
+        class="img-cover">
+
+      <div class="item-icon">
+        <span class="material-symbols-rounded">equalizer</span>
+      </div>
+    </button>
+  </li>
+  `;
+}
+
+/* PLAYLIST MODAL SIDEBAR TOGGLE
+ - tampilkan sidebar modal 'daftar putar' ketika mengklik tombol daftar putar di bilah aplikasi atas
+ - dan sembunyikan saat mengeklik overlay atau item daftar putar apa pun */
+
+const playlistSideModal = document.querySelector("[data-playlist]");
+const playlistTogglers = document.querySelectorAll("[data-playlist-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const togglePlaylist = function () {
+  playlistSideModal.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("modalActive");
+};
+
+addEventOnElements(playlistTogglers, "click", togglePlaylist);
+
+/* PLAYLIST ITEM
+ - hapus status aktif dari musik yang terakhir kali diputar
+ - dan tambahkan status aktif dalam musik yang diklik */
+
+const playlistItems = document.querySelectorAll("[data-playlist-item]");
+
+let currentMusic = 0;
+let lastPlayedMusic = 0;
+
+const changePlaylistItem = function () {
+  playlistItems[lastPlayedMusic].classList.remove("playing");
+  playlistItems[currentMusic].classList.add("playing");
+};
+
+addEventOnElements(playlistItems, "click", function () {
+  lastPlayedMusic = currentMusic;
+  currentMusic = Number(this.dataset.playlistItem);
+  changePlaylistItem();
+});
+
+/*PLAYER - mengubah semua informasi visual pada pemutar, berdasarkan musik saat in*/
+const playerBanner = document.querySelector("[data-player-banner]");
+const playerTitle = document.querySelector("[data-title]");
+const playerAlbum = document.querySelector("[data-album]");
+const playerYear = document.querySelector("[data-year]");
+const playerArtist = document.querySelector("[data-artist]");
+
+const audioSource = new Audio(musicData[currentMusic].musicPath);
+
+const changePlayerInfo = function () {
+  playerBanner.src = musicData[currentMusic].posterUrl;
+  playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
+  document.body.style.backgroundImage = `url(${musicData[currentMusic].backgroundImage})`;
+  playerTitle.textContent = musicData[currentMusic].title;
+  playerAlbum.textContent = musicData[currentMusic].album;
+  playerYear.textContent = musicData[currentMusic].year;
+  playerArtist.textContent = musicData[currentMusic].artist;
+
+  audioSource.src = musicData[currentMusic].musicPath;
+
+  audioSource.addEventListener("loadeddata", updateDuration);
+  playMusic();
+};
+
+addEventOnElements(playlistItems, "click", changePlayerInfo);
+
+/** update player duration */
+const playerDuration = document.querySelector("[data-duration]");
+const playerSeekRange = document.querySelector("[data-seek]");
+
+/** pass seconds and get timecode format */
+const getTimecode = function (duration) {
+  const minutes = Math.floor(duration / 60);
+  const seconds = Math.ceil(duration - minutes * 60);
+  const timecode = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  return timecode;
+};
+
+const updateDuration = function () {
+  playerSeekRange.max = Math.ceil(audioSource.duration);
+  playerDuration.textContent = getTimecode(Number(playerSeekRange.max));
+};
+
+audioSource.addEventListener("loadeddata", updateDuration);
+
+/* PLAY MUSIC - putar dan jeda musik saat mengklik tombol putar*/
+const playBtn = document.querySelector("[data-play-btn]");
+
+let playInterval;
+
+const playMusic = function () {
+  if (audioSource.paused) {
+    audioSource.play();
+    playBtn.classList.add("active");
+    playInterval = setInterval(updateRunningTime, 500);
+  } else {
+    audioSource.pause();
+    playBtn.classList.remove("active");
+    clearInterval(playInterval);
+  }
+};
+
+playBtn.addEventListener("click", playMusic);
+
+/*perbarui waktu berjalan saat memutar musik */
+const playerRunningTime = document.querySelector("[data-running-time");
+
+const updateRunningTime = function () {
+  playerSeekRange.value = audioSource.currentTime;
+  playerRunningTime.textContent = getTimecode(audioSource.currentTime);
+
+  updateRangeFill();
+  isMusicEnd();
+};
+
+/*RANGE FILL WIDTH - ubah lebar 'rangeFill', sambil mengubah nilai rentang*/
+const ranges = document.querySelectorAll("[data-range]");
+const rangeFill = document.querySelector("[data-range-fill]");
+
+const updateRangeFill = function () {
+  let element = this || ranges[0];
+
+  const rangeValue = (element.value / element.max) * 100;
+  element.nextElementSibling.style.width = `${rangeValue}%`;
+};
+
+addEventOnElements(ranges, "input", updateRangeFill);
+
+/*SEEK MUSIC - mencari musik sambil mengubah jangkauan pencarian pemain*/
+const seek = function () {
+  audioSource.currentTime = playerSeekRange.value;
+  playerRunningTime.textContent = getTimecode(playerSeekRange.value);
+};
+
+playerSeekRange.addEventListener("input", seek);
+
+/*END MUSIC*/
+/* AUTO PLAY NEXT MUSIC WHEN CURRENT MUSIC ENDS */
+audioSource.addEventListener("ended", function () {
+  skipNext();
+});
+
+const isMusicEnd = function () {
+  if (audioSource.ended) {
+    playBtn.classList.remove("active");
+    audioSource.currentTime = 0;
+    playerSeekRange.value = audioSource.currentTime;
+    playerRunningTime.textContent = getTimecode(audioSource.currentTime);
+    updateRangeFill();
+  }
+};
+
+/*SKIP TO NEXT MUSIC*/
+const playerSkipNextBtn = document.querySelector("[data-skip-next]");
+
+const skipNext = function () {
+  lastPlayedMusic = currentMusic;
+
+  if (isShuffled) {
+    shuffleMusic();
+  } else {
+    currentMusic >= musicData.length - 1 ? (currentMusic = 0) : currentMusic++;
+  }
+
+  changePlayerInfo();
+  changePlaylistItem();
+};
+
+playerSkipNextBtn.addEventListener("click", skipNext);
+
+/*SKIP TO PREVIOUS MUSIC*/
+const playerSkipPrevBtn = document.querySelector("[data-skip-prev]");
+
+const skipPrev = function () {
+  lastPlayedMusic = currentMusic;
+
+  if (isShuffled) {
+    shuffleMusic();
+  } else {
+    currentMusic <= 0 ? (currentMusic = musicData.length - 1) : currentMusic--;
+  }
+
+  changePlayerInfo();
+  changePlaylistItem();
+};
+
+playerSkipPrevBtn.addEventListener("click", skipPrev);
+
+/*Acak MUSIC - get random number for shuffle */
+const getRandomMusic = () => Math.floor(Math.random() * musicData.length);
+
+const shuffleMusic = () => (currentMusic = getRandomMusic());
+
+const playerShuffleBtn = document.querySelector("[data-shuffle]");
+let isShuffled = false;
+
+const shuffle = function () {
+  playerShuffleBtn.classList.toggle("active");
+
+  isShuffled = isShuffled ? false : true;
+};
+
+playerShuffleBtn.addEventListener("click", shuffle);
+
+/*REPEAT MUSIC*/
+const playerRepeatBtn = document.querySelector("[data-repeat]");
+
+const repeat = function () {
+  if (!audioSource.loop) {
+    audioSource.loop = true;
+    this.classList.add("active");
+  } else {
+    audioSource.loop = false;
+    this.classList.remove("active");
+  }
+};
+
+playerRepeatBtn.addEventListener("click", repeat);
+
+/* MUSIC VOLUME - menambah atau mengurangi volume musik saat mengubah volume*/
+const playerVolumeRange = document.querySelector("[data-volume]");
+const playerVolumeBtn = document.querySelector("[data-volume-btn]");
+
+const changeVolume = function () {
+  audioSource.volume = playerVolumeRange.value;
+  audioSource.muted = false;
+
+  if (audioSource.volume <= 0.1) {
+    playerVolumeBtn.children[0].textContent = "volume_mute";
+  } else if (audioSource.volume <= 0.5) {
+    playerVolumeBtn.children[0].textContent = "volume_down";
+  } else {
+    playerVolumeBtn.children[0].textContent = "volume_up";
+  }
+};
+
+playerVolumeRange.addEventListener("input", changeVolume);
+
+/*MUTE MUSIC */
+const muteVolume = function () {
+  if (!audioSource.muted) {
+    audioSource.muted = true;
+    playerVolumeBtn.children[0].textContent = "volume_off";
+  } else {
+    changeVolume();
+  }
+};
+
+playerVolumeBtn.addEventListener("click", muteVolume);
+
+/*lirik*/
